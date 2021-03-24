d25629e645a5fa8165469e6ce4bf496ad4027f75
jsw <allmie@naver.com>
2021-03-24 19:52:36 +0900

    search pug modify: get query

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,11 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) =>
-  res.render("search", { pageTitle: "Search" });
+export const search = (req, res) => {
+  const {
+    query: { searchTerm },
+  } = req;
+
+  res.render("search", { pageTitle: "Search", searchTerm });
+};
 export const upload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
 export const videoDetail = (req, res) =>

15d8bdece524ba52e3048d84966efddd69b29a57
jsw <allmie@naver.com>
2021-03-24 18:23:13 +0900

    pug install

--- src/app.js
+++ src/app.js
@@ -10,12 +10,13 @@ import routes from "../routes";
 
 const app = express();
 
+app.use(helmet());
+app.use(morgan("dev"));
+app.set("view engine", "pug");
+app.set("views", "src/views");
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(helmet());
-app.use(morgan("dev"));
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const home = (req, res) => res.send("home");
-export const search = (req, res) => res.send("search");
-export const upload = (req, res) => res.send("upload");
-export const videoDetail = (req, res) => res.send("video Detail");
-export const videoEdit = (req, res) => res.send("video Edit");
-export const videoDelete = (req, res) => res.send("video Delete");
+export const home = (req, res) => res.render("home");
+export const search = (req, res) => res.render("search");
+export const upload = (req, res) => res.render("upload");
+export const videoDetail = (req, res) => res.render("video Detail");
+export const videoEdit = (req, res) => res.render("video Edit");
+export const videoDelete = (req, res) => res.render("video Delete");

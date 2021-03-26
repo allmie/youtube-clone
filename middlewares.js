6309fd19f7f7a34938e02f1d9d7048bf2138260c
jsw <allmie@naver.com>
2021-03-26 11:26:02 +0900

    router, controller, pug modify: -detail, login

--- middlewares.js
+++ middlewares.js
@@ -4,5 +4,9 @@ import routes from "./src/routes";
 export const localsMiddleware = (req, res, next) => {
   res.locals.routes = routes;
   res.locals.siteName = "Cloning-youtube";
+  res.locals.user = {
+    isAuthenticated: true,
+    id: 1,
+  };
   next();
 };
